72191ce4ac9b7d7de235984f71bccdab168eeeee
Emily Quevedo <37810404+EmilyQue@users.noreply.github.com>
2019-02-03 23:09:33 -0700

    Add files via upload

---
+++ assignment2a/src/assignment2a/TestServlet.java
@@ -0,0 +1,69 @@
+package assignment2a;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TestServlet
+ */
+@WebServlet("/TestServlet")
+public class TestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TestServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		super.destroy();
+	}
+
+	@Override
+	public void init() throws ServletException {
+		// TODO Auto-generated method stub
+		super.init();
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		String firstName = request.getParameter("firstname");
+		String lastName = request.getParameter("lastname");
+		
+		if (firstName == null || firstName.isEmpty() || lastName == null || lastName.isEmpty()) {
+			request.getRequestDispatcher("TestError.jsp").forward(request, response);
+		}
+		
+		else {
+			request.setAttribute("firstname", firstName);
+			request.setAttribute("lastname", lastName);
+		
+			request.getRequestDispatcher("TestResponse.jsp").forward(request, response);
+		}
+		//response.getWriter().append("Hello, my name is: ").append("Emily" + " " +"Quevedo");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+
+
+}
